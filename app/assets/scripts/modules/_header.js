b3b132301a73ef372a04702f0ff64a0d256efacb
sergio <sacst.py@gmail.com>
2020-10-29 20:43:14 -0300

    go web

--- app/assets/scripts/modules/_header.js
+++ app/assets/scripts/modules/_header.js
@@ -5,24 +5,28 @@ class Header {
         this.idioma = document.querySelector(".idioma");
         this.barra = document.getElementById("barra_x");
         this.sub = document.querySelectorAll(".has-submenu");
+        this.esp = document.querySelector(".btnEs");
+        this.eng = document.querySelector(".btnEn");
+        
         this.events();
     }
 
     toggleMenu() {
-        if (this.menu.classList.contains("active")) {
-            
-            this.menu.classList.remove("active");
-            this.idioma.classList.remove("idioma-visible")
-            this.idioma.classList.add("idioma");
-            this.barra.classList.remove("fa-times");
-            this.barra.classList.add("fa-bars");
-        } else {
-            this.menu.classList.add("active");
-            // adds the close (x) icon
-            this.idioma.classList.remove("idioma")
-            this.idioma.classList.add("idioma-visible");
-            this.barra.classList.remove("fa-bars");
-            this.barra.classList.add("fa-times");
+        if (window.innerWidth < 800) {
+            if (this.menu.classList.contains("active")) {
+                this.menu.classList.remove("active");
+                this.idioma.classList.remove("idioma-visible")
+                this.idioma.classList.add("idioma");
+                this.barra.classList.remove("fa-times");
+                this.barra.classList.add("fa-bars");
+            } else {
+                this.menu.classList.add("active");
+                // adds the close (x) icon
+                this.idioma.classList.remove("idioma")
+                this.idioma.classList.add("idioma-visible");
+                this.barra.classList.remove("fa-bars");
+                this.barra.classList.add("fa-times");
+            }
         }
     }
 
@@ -39,8 +43,13 @@ class Header {
     events(){
         this.toggle.addEventListener("click", () => this.toggleMenu())
         this.sub.forEach(el => el.addEventListener("click", () => this.toggleSubMenu(el)))
-        this.sub.forEach(el => el.addEventListener("mouseover", () => this.toggleSubMenu(el)))
-        this.sub.forEach(el => el.addEventListener("mouseout", () => this.toggleSubMenu(el)))
+        if (window.innerWidth > 800 ) {
+            this.sub.forEach(el => el.addEventListener("mouseover", () => this.toggleSubMenu(el)))
+            this.sub.forEach(el => el.addEventListener("mouseout", () => this.toggleSubMenu(el)))   
+        }
+        this.esp.addEventListener("click", () => this.pintaridiomaesp())
+        this.eng.addEventListener("click", () => this.pintaridiomaeng())
+        console.log(this.sub)
     }
 
     closemenu(){
@@ -49,7 +58,20 @@ class Header {
         this.barra.classList.add("fa-bars");
         this.idioma.classList.remove("idioma-visible")
         this.idioma.classList.add("idioma");
+    }
 
+    pintaridiomaesp(){
+        if (this.eng.classList.contains("btnActive")) {
+            this.eng.classList.remove("btnActive");
+            this.esp.classList.add("btnActive");   
+        }
+    }
+
+    pintaridiomaeng(){
+        if (this.esp.classList.contains("btnActive")) {
+            this.esp.classList.remove("btnActive");
+            this.eng.classList.add("btnActive");   
+        }
     }
    
 }
